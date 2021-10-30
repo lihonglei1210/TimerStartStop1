8563ebb5d32d1433e252a44651c1a45660f77ada
1730226392@qq.com <1730226392@qq.com>
2021-10-30 08:46:27 +0800

    添加项目文件。

---
+++ 定时启动关闭应用程序/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace 定时启动关闭应用程序
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.MyNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "秒",
+            "分",
+            "时"});
+            this.comboBox1.Location = new System.Drawing.Point(218, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(358, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "D:\\AiheDataServerV53_Demo\\AiheDataServer.exe";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1800";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 23);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MyNotifyIcon
+            // 
+            this.MyNotifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.MyNotifyIcon.BalloonTipTitle = "Auto";
+            this.MyNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("MyNotifyIcon.Icon")));
+            this.MyNotifyIcon.Text = "Auto";
+            this.MyNotifyIcon.Visible = true;
+            this.MyNotifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 64);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Auto";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon MyNotifyIcon;
+    }
+}
+
---
+++ 定时启动关闭应用程序/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace 定时启动关闭应用程序
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            button1_Click(null,null);
+        }
+       
+        static int jishi;
+        static Process proc;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "启动")
+            {
+                try
+                {
+                    proc_Start();
+                    
+                    button1.Text = "停止";
+                    notifyIcon1_MouseDoubleClick(null, null);
+                }
+                catch (ArgumentException ex)
+                {
+                    
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+            else
+            {
+                timer1.Enabled = false;
+                try
+                {
+                    proc.Kill();
+                    
+                }
+                catch (Exception)
+                {
+
+                   
+                }
+                jishi = 0;
+                label1.Text = jishi.ToString();
+                button1.Text = "启动";
+            }
+
+           
+        }
+        void proc_AutoStart(object sender, EventArgs e)
+        {
+            Thread.Sleep(5000);
+            proc.EnableRaisingEvents = false;
+            proc_Start();
+        }
+
+        private void proc_Start()
+        {
+            proc = new Process();
+            proc.StartInfo.FileName = textBox1.Text;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            proc.Start();
+
+            //proc.
+            timer1.Enabled = true;
+            timer1.Start();
+            
+        }
+      
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+              jishi = jishi+1;
+              label1.Text = jishi.ToString();
+              if (textBox2.Text!="")
+              switch (comboBox1.Text)
+              {
+                  case "秒":
+                    int a= Convert.ToInt32(textBox2.Text);
+                      if (jishi > a+1 || jishi == a+1)
+                      {
+                            
+                            jishi = 0;
+                            label1.Text = jishi.ToString();
+                            timer1.Enabled = false;
+                            try
+                            {
+                                proc.Kill();
+                            }
+                            catch (Exception)
+                            { 
+
+                                
+                            }
+                           
+                            proc_AutoStart(null, null);
+                            
+                        }
+                          break;
+                  case "分":
+                      int b = Convert.ToInt32(textBox2.Text);
+                      b = b * 60;
+                      if (jishi > b+1 || jishi == b+1)
+                      {
+                            
+                            jishi = 0;
+                            label1.Text = jishi.ToString();
+                            timer1.Enabled = false;
+                            try
+                            {
+                                proc.Kill();
+                            }
+                            catch (Exception)
+                            {
+
+                                
+                            }
+
+                            proc_AutoStart(null, null);
+
+                      }
+                      break;
+
+                  case "时":
+                      int c = Convert.ToInt32(textBox2.Text);
+                      c = c*60*60;
+                      if (jishi > c+1 || jishi == c+1)
+                      {
+                          
+                            jishi = 0;
+                            label1.Text = jishi.ToString();
+                            timer1.Enabled = false;
+                            try
+                            {
+                                proc.Kill();
+                            }
+                            catch (Exception)
+                            {
+
+                               
+                            }
+
+                            proc_AutoStart(null, null);
+                      }
+                      break;
+              }
+            if (button2.BackColor == Color.Green)
+            {
+                button2.BackColor = Color.Red;
+            }
+            else
+            {
+                button2.BackColor = Color.Green;
+            }
+         
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.Visible)
+            {
+                this.Hide();
+            }
+            else
+            {
+                this.Show();
+            }
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            notifyIcon1_MouseDoubleClick(null,null);
+        }
+    }
+}
